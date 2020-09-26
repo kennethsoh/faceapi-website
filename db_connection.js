c1b1a507bf7a947c3edd23d32fffc29343d74727
Kenneth Soh <34814795+kennethsoh@users.noreply.github.com>
2020-09-26 21:10:26 +0800

    Update db_connection.js

--- db_connection.js
+++ db_connection.js
@@ -2,15 +2,15 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "127.0.0.1",
-  port:"3306",
-  user:"root",  // mysql database login username
-  password: "", // mysql database login password 
+  port:"3306",      // mysql database port. 3306 is default
+  user:"root",      // mysql database login username
+  password: "",     // mysql database login password 
   database:"logs"
 });
 
 con.connect(function(err){
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Database Connected!");
 });
 
 module.exports = con;
