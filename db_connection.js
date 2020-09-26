18d20362bbf1acbabc52aa7b0a53fc17c3d52b34
Kenneth Soh <34814795+kennethsoh@users.noreply.github.com>
2020-09-26 21:00:05 +0800

    Update db_connection.js

--- db_connection.js
+++ db_connection.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "127.0.0.1",
   port:"3306",
-  user:"root",
-  password: "kenneth8754",
+  user:"root",  // mysql database login username
+  password: "", // mysql database login password 
   database:"logs"
 });
 
